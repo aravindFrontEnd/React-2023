5770085e06f928ffc2220c77d1665afd305963b4
Aravind Ganesh <91867114+Epsilon-Aravind@users.noreply.github.com>
2023-04-28 22:23:50 +0530

    React js fundamentals full

---
+++ src/cars.js
@@ -0,0 +1,26 @@
+import img1 from './images/hyundai-verna.avif';
+import img2 from './images/mahendra-car.avif';
+import img3 from './images/maruthi.avif';
+export const cars = [
+  {
+    author: 'Hyundai',
+    title:
+      'The price  starts at Rs. 7.80 Lakh and goes upto Rs. 14.35 Lakh',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Mahendra',
+    title:
+      'The price of  at Rs. 17.80 Lakh and goes upto Rs. 24.35 Lakh',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Maruthi',
+    title:
+      'The price starts Rs. 47.80 Lakh and goes upto Rs. 54.35 Lakh',
+    img: img3,
+    id: 3,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './images/simbu.jpg';
-
 import './index.css';
-
-// import { books } from './books';
-// import Book from './Book';
-
-function Index() {
+import {cars} from './cars'
+import Car from './Car';
 
 
-    const parent = {
-      marginInline: 'auto',
-      maxWidth: '700px',
-    }
 
+
+
+function CarList() {
   return (
-    <>
-      <div style={parent}>
-        <p>
-          Inime <b>React la</b> ena panranu matum parunga
-        </p>
-        <img src={logo} alt='simbu' />
-        <p style={{textAlign:'center'}}>vidave matan !! vidave matan!!</p>
-      </div>
-    </>
+    <section className='carlist'>
+      <h1> Best Cars in India</h1>
+{/* whole cars data */}
+      {cars.map((car, index) => {
+        return (
+          // Individual car
+        <Car {...car}
+        key={car.id} />
+        )
+      })}
+    </section>
   )
 }
 
+
+// const Car = ({ img, title, author}) => {
+
+//   return (
+//     <article className='car'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author} </h4>
+//     </article>
+//   )
+// }
+
+
+// const EventExamples = () => {
+//   return (
+//     <section>
+//       <form>
+//         <h2>Typical Form</h2>
+//         <input
+//           type='text'
+//           name='example'
+//           onChange={(e) => {
+//             console.log(e.target.value);
+//             console.log(e.target.name)}}
+//           style={{ margin: '1rem 0' }}
+//         />
+//       </form>
+//       <button onClick={() => console.log('you clicked me')}>click me</button>
+//     </section>
+//   )
+// }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Index />);
+root.render(<CarList />)
